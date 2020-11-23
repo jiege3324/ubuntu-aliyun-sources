4371dcf39785ae0a8117ea886eb56c4b28eac8dc
jiege3324 <1206538127@qq.com>
2020-11-23 15:18:54 +0800

    Update change.sh

--- change.sh
+++ change.sh
@@ -29,10 +29,10 @@ if [ $(id -u) -ne 0 ]; then
     echo "Need Root!"
     exit 0
 fi
-#备份文件
+#bak
 cp /etc/apt/sources1604.list /etc/apt/sources1604.list.bak
-#替换服务器
+#replace
 sed -i 's/http:\/\/archive.ubuntu.com/http:\/\/mirrors.aliyun.com/g' /etc/apt/sources1604.list
 sed -i 's/http:\/\/security.ubuntu.com/http:\/\/mirrors.aliyun.com/g' /etc/apt/sources1604.list
-#更新源
+#update
 apt-get update
